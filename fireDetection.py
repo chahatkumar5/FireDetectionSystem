4528b414318cd5e6402450d290d9025a3e64a8b3
chahatkumar5 <97453388+chahatkumar5@users.noreply.github.com>
2024-06-30 08:15:58 +0530

    Update fireDetection.py

--- fireDetection.py
+++ fireDetection.py
@@ -1,40 +1,40 @@
-import cv2         # Library for openCV
-import threading   # Library for threading -- which allows code to run in backend
-import playsound   # Library for alarm sound
-import smtplib     # Library for email sending
+import cv2         
+import threading   
+import playsound   
+import smtplib     
 
-fire_cascade = cv2.CascadeClassifier('fire_detection_cascade_model.xml') # To access xml file which includes positive and negative images of fire. (Trained images)
-                                                                         # File is also provided with the code.
+fire_cascade = cv2.CascadeClassifier('fire_detection_cascade_model.xml') 
+                                                                         
 
-vid = cv2.VideoCapture(0) # To start camera this command is used "0" for laptop inbuilt camera and "1" for USB attahed camera
-runOnce = False # created boolean
+vid = cv2.VideoCapture(0) 
+runOnce = False 
 
-def play_alarm_sound_function(): # defined function to play alarm post fire detection using threading
-    playsound.playsound('fire_alarm.mp3',True) # to play alarm # mp3 audio file is also provided with the code.
-    print("Fire alarm end") # to print in consol
+def play_alarm_sound_function():
+    playsound.playsound('fire_alarm.mp3',True) 
+    print("Fire alarm end") 
 
-def send_mail_function(): # defined function to send mail post fire detection using threading
+def send_mail_function(): 
     
-    recipientmail = "chahatkumar513@gmail.com" # recipients mail
-    recipientmail = recipientmail.lower() # To lower case mail
+    recipientmail = "chahatkumar513@gmail.com" 
+    recipientmail = recipientmail.lower() 
     
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
         server.starttls()
         server.login("add senders mail", 'add senders password') # Senders mail ID and password
-        server.sendmail('add recipients mail', recipientmail, "Warning fire accident has been reported") # recipients mail with mail message
-        print("Alert mail sent sucesfully to {}".format(recipientmail)) # to print in consol to whome mail is sent
-        server.close() ## To close server
+        server.sendmail('add recipients mail', recipientmail, "Warning fire accident has been reported")
+        print("Alert mail sent sucesfully to {}".format(recipientmail)) 
+        server.close() 
         
     except Exception as e:
-        print(e) # To print error if any
+        print(e)
 		
 while(True):
     Alarm_Status = False
-    ret, frame = vid.read() # Value in ret is True # To read video frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # To convert frame into gray color
-    fire = fire_cascade.detectMultiScale(frame, 1.2, 5) # to provide frame resolution
+    ret, frame = vid.read() 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    fire = fire_cascade.detectMultiScale(frame, 1.2, 5) 
 
     ## to highlight fire with square 
     for (x,y,w,h) in fire:
@@ -43,11 +43,11 @@ while(True):
         roi_color = frame[y:y+h, x:x+w]
 
         print("Fire alarm initiated")
-        threading.Thread(target=play_alarm_sound_function).start()  # To call alarm thread
+        threading.Thread(target=play_alarm_sound_function).start()  
 
         if runOnce == False:
             print("Mail send initiated")
-            threading.Thread(target=send_mail_function).start() # To call alarm thread
+            threading.Thread(target=send_mail_function).start() 
             runOnce = True
         if runOnce == True:
             print("Mail is already sent once")
